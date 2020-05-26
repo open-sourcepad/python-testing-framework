6043d95a85c93201eff46cccdbc8cd1a4cb36079
dyao <djy@sourcepad.com>
2020-05-26 18:13:28 +0800

    added new selenium implementation

--- libs/selenium_request.py
+++ libs/selenium_request.py
@@ -8,17 +8,21 @@ class SeleniumError(Exception):
 
 class Selenium:
     def __init__(self, **kwargs):
+        self.method = kwargs.get('method', 'get')
+        self.data = kwargs.get('data')
+        self.headers = kwargs.get('headers')
         self.options = Options()
         self._options()
 
     def browser(self, url):
         try:
             driver = Firefox(options=self.options)
-            driver.get(url)
-
+            headers = self.headers
+            result = driver.request(self.method, url, data=self.data)
+            import pry;pry()
             return driver
         except Exception as e:
-            raise SeleniumUatError(f"Error setting instance of Firefox.\nError: {e}")
+            raise SeleniumError(f"Error setting instance of Firefox.\nError: {e}")
 
     def _options(self):
         self.options.headless = True
